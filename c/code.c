607ef96b675a245eae4024221217ceb23d3bbba0
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-12-02 14:06:16 -0800

    Add broken linked list

--- c/code.c
+++ c/code.c
@@ -5,6 +5,48 @@
 #include <ctype.h>
 #include <string.h>
 
+struct name_node {
+  char *name;
+  struct name_node *next;
+};
+
+void print_all_entries(struct name_node *l)
+{
+  struct name_node *iter = l;
+  while (iter != NULL) {
+    printf("Data %s\n", iter->name);
+    iter = iter->next;
+  }
+
+}
+
+void linked_list_example(void)
+{
+  struct name_node *head = malloc(sizeof(struct name_node));
+  struct name_node *iter_node = head;
+  char *names[] = {"Edgar", "Gohar", "Robert", "Kalipso", NULL};
+  size_t spot = 0;
+  char *iter = names[spot];
+
+  while (true) {
+    if (names[spot] == NULL) {
+      break;
+    } else {
+      if (iter != NULL) {
+        iter_node->name = malloc(strlen(iter) + 1);
+        strcpy(iter_node->name, iter);
+        spot++;
+        iter = names[spot];
+        iter_node->next = malloc(sizeof(struct name_node));
+        iter_node = iter_node->next;
+      }
+    }
+  }
+
+  print_all_entries(head);
+}
+
+
 size_t strlen_(char *n)
 {
   size_t length = 0;
@@ -22,6 +64,7 @@ size_t strlen_(char *n)
 
 int main (int argc, char **argv)
 {
+  linked_list_example();
   printf("Test length: %zu\n", strlen_("Hello World"));
   return 0;
 }
